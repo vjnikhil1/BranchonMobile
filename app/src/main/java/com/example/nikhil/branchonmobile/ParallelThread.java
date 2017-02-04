2906af9a6858af7a5cbb8112367be5f7ad5d63d4
Nikhil <johnnikhil95@gmail.com>
2017-02-04 22:48:12 +0530

    Added Asynchronous Task

---
+++ app/src/main/java/com/example/nikhil/branchonmobile/ParallelThread.java
@@ -0,0 +1,86 @@
+package com.example.nikhil.branchonmobile;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by Nikhil on 04-02-2017.
+ */
+
+class ParallelThread extends AsyncTask<String, Void, String> {
+    Context c;
+    String result = "";
+    ParallelThread(Context c){
+        this.c = c;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String route = params[0];
+        String url_register = "http://bom.pe.hu/register.php";
+        if (route == "register") {
+            String first_name = params[1];
+            String last_name = params[2];
+            String email = params[3];
+            String mobile = params[4];
+            String pan = params[5];
+            String address = params[6];
+            try {
+                URL url = new URL(url_register);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("POST");
+                con.setDoOutput(true);
+                con.setDoInput(true);
+                OutputStream os = con.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                String post = URLEncoder.encode("first_name", "UTF-8") + "=" + URLEncoder.encode(first_name, "UTF-8") + "&" +
+                        URLEncoder.encode("last_name", "UTF-8") + "=" + URLEncoder.encode(last_name, "UTF-8") + "&" +
+                        URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8") + "&" +
+                        URLEncoder.encode("mobile", "UTF-8") + "=" + URLEncoder.encode(mobile, "UTF-8") + "&" +
+                        URLEncoder.encode("pan", "UTF-8") + "=" + URLEncoder.encode(pan, "UTF-8") + "&" +
+                        URLEncoder.encode("address", "UTF-8") + "=" + URLEncoder.encode(address, "UTF-8");
+                bufferedWriter.write(post);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                os.close();
+                InputStream is = con.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                result = "";
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                is.close();
+                con.disconnect();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+        Toast.makeText(c,result,Toast.LENGTH_LONG).show();
+    }
+}
