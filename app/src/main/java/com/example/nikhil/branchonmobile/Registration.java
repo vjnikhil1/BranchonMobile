79d419148cea93e61ed854b0afcddc5faa172b93
Nikhil <johnnikhil95@gmail.com>
2017-02-05 15:56:06 +0530

    Added Image Capture and upload from Camera

--- app/src/main/java/com/example/nikhil/branchonmobile/Registration.java
+++ app/src/main/java/com/example/nikhil/branchonmobile/Registration.java
@@ -1,8 +1,13 @@
 package com.example.nikhil.branchonmobile;
 
 import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,16 +16,20 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+
 public class Registration extends AppCompatActivity {
-    private Button register;
+    private Button register,upload;
     private RadioGroup rg;
     private RadioButton rb;
     private EditText name,lname,email,mobile,pan,address,acctype;
+    private String img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         register = (Button) findViewById(R.id.button2);
+        upload = (Button) findViewById(R.id.button3);
         rg = (RadioGroup) findViewById(R.id.radioGroup);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,10 +45,44 @@ public class Registration extends AppCompatActivity {
                 ParallelThread pt = new ParallelThread(Registration.this);
                 pt.execute("register",name.getText().toString(),lname.getText().toString(),
                         email.getText().toString(),mobile.getText().toString(),rb.getText().toString(),
-                        "",address.getText().toString());
+                        img,address.getText().toString());
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                String pickTitle = "Select or take a new Picture"; // Or get from strings.xml
+                Intent chooserIntent = Intent.createChooser(pickPhoto, pickTitle);
+                chooserIntent.putExtra
+                        (
+                                Intent.EXTRA_INITIAL_INTENTS,
+                                new Intent[] { takePhotoIntent }
+                        );
+
+                startActivityForResult(chooserIntent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //if(requestCode==PICK_IMAGE_REQUEST)
+        Log.e("x",""+resultCode);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            /*Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG,100,bo);
+            byte[] ba=bo.toByteArray();
+            img = Base64.encodeToString(ba, Base64.DEFAULT);
+            Log.e("image",img);*/
+        }
     }
 }
