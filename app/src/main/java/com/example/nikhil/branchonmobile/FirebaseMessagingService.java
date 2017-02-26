b5562fb252fa5793e676952c56d5371c9c6159b0
Nikhil <johnnikhil95@gmail.com>
2017-02-27 00:52:44 +0530

    Transfering Funds Notification added

--- app/src/main/java/com/example/nikhil/branchonmobile/FirebaseMessagingService.java
+++ app/src/main/java/com/example/nikhil/branchonmobile/FirebaseMessagingService.java
@@ -3,6 +3,9 @@ package com.example.nikhil.branchonmobile;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.RemoteMessage;
@@ -20,14 +23,16 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     private void showNotification(String message) {
         Intent i = new Intent(this,MainActivity.class);
         i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0,i,PendingIntent.FLAG_UPDATE_CURRENT);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
                 .setAutoCancel(true)
-                .setContentTitle("FCM Test")
+                .setContentTitle("Fund Transfer Successful")
                 .setContentText(message)
-                .setSmallIcon(R.drawable.ic_launcher)
+                .setSmallIcon(R.drawable.ic_menu_send)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher))
+                .setSound(uri)
                 .setContentIntent(pendingIntent);
 
         NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
