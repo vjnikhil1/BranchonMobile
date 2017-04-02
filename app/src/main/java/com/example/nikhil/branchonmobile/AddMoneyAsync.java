d460da4133bc6678931073ec9e0d0da837213ff3
Nikhil <johnnikhil95@gmail.com>
2017-04-02 16:05:44 +0530

    Added Payment Gateway Staging

---
+++ app/src/main/java/com/example/nikhil/branchonmobile/AddMoneyAsync.java
@@ -0,0 +1,25 @@
+package com.example.nikhil.branchonmobile;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Nikhil on 02-04-2017.
+ */
+
+public class AddMoneyAsync extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected void onPreExecute() {
+
+    }
+
+    @Override
+    protected String doInBackground(String[] params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String o) {
+
+    }
+}
